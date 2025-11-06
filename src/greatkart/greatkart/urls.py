2ba592eeeaecbe632ce0567514e1d47c325582de
Asma sherin <asmasherin37@gmail.com>
2025-11-06 14:20:56 +0530

    store and store funtionalities

--- src/greatkart/greatkart/urls.py
+++ src/greatkart/greatkart/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myapp.urls'),),
     path('store/',include('store.urls')),
+    path('cart/',include('carts.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 

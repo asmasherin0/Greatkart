63753b9448e8c8718bfcf45301a61dcf92f5f70d
Asma sherin <asmasherin37@gmail.com>
2025-11-11 12:22:44 +0530

    django authentication

--- src/greatkart/greatkart/urls.py
+++ src/greatkart/greatkart/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myapp.urls'),),
     path('store/',include('store.urls')),
-    path('cart/',include('carts.urls'))
+    path('cart/',include('carts.urls')),
+    path('accounts/',include('accounts.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
